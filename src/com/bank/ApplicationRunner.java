063b74a4cb47ec9533c5421ab5ce9b8948038828
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-08-28 16:08:05 +0200

    printMenu method

--- src/com/bank/ApplicationRunner.java
+++ src/com/bank/ApplicationRunner.java
@@ -1,7 +1,10 @@
 package com.bank;
 
+import com.bank.service.Bank;
+
 public class ApplicationRunner {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Bank application");
+        Bank bank = new Bank();
+        bank.run();
     }
 }
---
+++ src/com/bank/service/Bank.java
@@ -0,0 +1,16 @@
+package com.bank.service;
+
+public class Bank {
+
+    public void run(){
+        printMenu();
+    }
+
+    public void printMenu(){
+        System.out.println("0 - Exit");
+        System.out.println("1 - Add new costumer");
+        System.out.println("2 - Print all costumers");
+        System.out.println("3 - Search and print costumer by name");
+        System.out.println("4 - Search and print costumer by name");
+    }
+}
